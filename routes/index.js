aca61479aec15d0fd25fbccedb5cbaeeefa3ca2b
Tim Saunders <tim@sauntimo.org>
2018-10-27 19:50:42 +0100

    Added bootstrap, landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+var data = {
+	"title"       : "Tech Jobs Bristol",
+	"intro"       : "A transparent jobs board by developers for developers.",
+	"explanation" : `No recuitment agencies, ever. Just people with tech jobs 
+		and people looking for them. Simple.`
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', data);
 });
 
 module.exports = router;
